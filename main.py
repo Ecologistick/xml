8cedb01519bc51138aebe4a6282c9deda1c21b83
Ecologistick <vadikzelenskiy@gmail.com>
2020-11-09 20:26:16 +0000

    split into separate functions

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from pprint import pprint
 import xml.etree.ElementTree as ET
-def top(file):
+
+def open_file(file):
   tree = ET.parse(file)
+  return tree
+
+def picking(tree):
   counters = {}
   for description in tree.iterfind('channel/item/description'):
     for word in description.text.lower().split():
@@ -11,6 +15,9 @@ def top(file):
         counters[word] = 1
       else:
         counters[word] += 1
+  return counters
+
+def top(counters):
   return sorted(counters.items(), key=lambda i: i[1], reverse=True)[:10]
 
-pprint(top('newsafr.xml'))
+pprint(top(picking(open_file('newsafr.xml'))))
